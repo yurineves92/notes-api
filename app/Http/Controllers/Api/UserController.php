01e3661233393c2d9001f95f054ba99e570b234e
Yuri Neves <yurineves92@gmail.com>
2023-05-11 13:02:37 -0300

    first update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,41 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function profile()
+    {
+        $user = Auth::user();
+        return response()->json(["user" => $user], 200);
+    }
+
+    public function update(Request $request)
+    {
+        $user = User::find(Auth::user()->id);
+        if (empty($user)) {
+            return response()->json(
+                [
+                    "data" => [
+                        "message" => "Usuário não encontrado.",
+                    ],
+                ],
+                200
+            );
+        }
+        $user->name = $request->name;
+        $user->save();
+        return response()->json(
+            [
+                "data" => [
+                    "message" => "Suas informações foram atualizadas.",
+                    "data" => $user,
+                ],
+            ],
+            200
+        );
+    }
 }
