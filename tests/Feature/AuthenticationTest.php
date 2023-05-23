57d4033bad49d68e644eb35dcfa6bc125e5b4bc6
Yuri Neves <yurineves92@gmail.com>
2023-05-22 22:26:28 -0300

    first test

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test if a user can login successfully by sending a POST request to the "login" route.
+     *
+     * @return void
+     */
+    public function testUserCanLogin()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $response = $this->post(route('auth.login'), [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'token_type' => 'Bearer',
+            // Add other expected response data here
+        ]);
+    }
+
+    /**
+     * Test if an unauthenticated user receives an error when trying to access authenticated routes.
+     *
+     * @return void
+     */
+    public function testUnauthenticatedUserCannotAccessAuthenticatedRoutes()
+    {
+        $response = $this->get(route('user.profile'));
+
+        $response->assertStatus(401);
+        // Add other assertions for the error response here
+    }
+}
